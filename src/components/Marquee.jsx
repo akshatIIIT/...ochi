5685442a5aff2e94fb2519d36d38b73f99934027
akshatIIIT <167337267+akshatIIIT@users.noreply.github.com>
2025-07-15 19:28:12 +0530

    Update Marquee.jsx

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React from 'react';
 import { motion } from "framer-motion";
+
 function Marquee() {
   return (
-    <div data-scroll data-scroll-section data-scroll-speed="-.2" className="w-full py-20 bg-[#004D43] rounded-tl-3xl rounded-tr-3xl">
-        <div  className=" text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap overflow-hidden gap-10">
-            <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity,duration:10}} className="text-[24vw] leading-none font-['Founders_Grotesk'] font-semibold uppercase pt-10 -mb-[7vw]">We are ochi</motion.h1>
-            <motion.h1 initial={{x:0}} animate={{x:"-100%"}} transition={{ease:"linear",repeat:Infinity,duration:10}} className="text-[24vw] leading-none font-['Founders_Grotesk'] font-semibold uppercase pt-10 -mb-[7vw]">we are ochi</motion.h1>
-        </div>
+    <div
+      data-scroll
+      data-scroll-section
+      data-scroll-speed="-.2"
+      className="w-full py-10 md:py-20 bg-[#004D43] rounded-tl-3xl rounded-tr-3xl"
+    >
+      <div className="text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap overflow-hidden gap-10">
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 10 }}
+          className="text-[16vw] md:text-[24vw] leading-none font-['Founders_Grotesk'] font-semibold uppercase pt-6 md:pt-10 -mb-[4vw] md:-mb-[7vw]"
+        >
+          We are ochi
+        </motion.h1>
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 10 }}
+          className="text-[16vw] md:text-[24vw] leading-none font-['Founders_Grotesk'] font-semibold uppercase pt-6 md:pt-10 -mb-[4vw] md:-mb-[7vw]"
+        >
+          we are ochi
+        </motion.h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Marquee
+export default Marquee;
